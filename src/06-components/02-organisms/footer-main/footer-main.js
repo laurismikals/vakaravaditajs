7217c1e8be9089667f766385b8a7aa6ef7e95edd
Lauris <lauris.mikals@gmail.com>
2018-02-19 10:34:31 +0200

    add nav social to footer

--- src/06-components/02-organisms/footer-main/footer-main.js
+++ src/06-components/02-organisms/footer-main/footer-main.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import NavSocial from '../../01-molecules/nav-social/nav-social';
+
 import textStyles from '../../00-atoms/text/text.css';
 import styles from './footer-main.css';
 
 export default () => (
   <footer className={styles.footerMain}>
     <div className="container">
-      <div className={textStyles.text}>
-        <p>&copy; Raivis Bružis visas tiesības aizsargātas</p>
+      <div className={styles.inner}>
+        <div className={textStyles.text}>
+          <p>&copy; Raivis Bružis visas tiesības aizsargātas</p>
+        </div>
+        <NavSocial />
       </div>
     </div>
   </footer>
