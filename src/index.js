ec673268ca368890fbece2ce4bd7de38e1941852
Lauris <lauris.mikals@gmail.com>
2018-02-19 11:12:43 +0200

    add modernizr

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import createHistory from 'history/createBrowserHistory';
 import AppContainer from 'react-hot-loader/lib/AppContainer'; // eslint-disable-line import/no-extraneous-dependencies
+
+import Modernizr from './00-assets/js-libs/modernizr-custom'; // eslint-disable-line no-unused-vars
+
 import Layout from './06-components/03-layout';
 import configureStore from './configureStore';
 
@@ -27,7 +30,7 @@ render(Layout);
 if (module.hot && process.env.NODE_ENV === 'development') {
   module.hot.accept('./06-components/03-layout', () => {
     // eslint-disable-next-line
-    const Layout = require('./06-components/03-layout').default
+    const Layout = require('./06-components/03-layout').default;
 
     render(Layout);
   });
