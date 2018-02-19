a61e85760c4f0499190a37a278f5602ac48ad576
Lauris <lauris.mikals@gmail.com>
2018-02-19 10:33:20 +0200

    add icon component

---
+++ src/06-components/00-atoms/icon/icon.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import svg4everybody from 'svg4everybody';
+
+import styles from './icon.css';
+
+const Icon = ({ className = '', icon }) => (
+  <svg className={`${styles.icon} ${className}`}>
+    <use
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      xlinkHref={`/dist/icons-sprite.svg#${icon}`}
+    />
+  </svg>
+);
+
+Icon.propTypes = {
+  icon: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+Icon.defaultProps = {
+  className: '',
+};
+
+export default Icon;
