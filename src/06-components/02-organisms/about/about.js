1d127852546647023b104decd5144b9ce833f0a7
Lauris <lauris.mikals@gmail.com>
2018-02-23 11:42:35 +0200

    connected about component to about reducer

--- src/06-components/02-organisms/about/about.js
+++ src/06-components/02-organisms/about/about.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import headingStyles from '../../00-atoms/heading/heading.css';
 import textStyles from '../../00-atoms/text/text.css';
 import styles from './about.css';
 
-const TEXT = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque eos eum ex ipsa nostrum possimus repudiandae, totam veniam veritatis! A ad adipisci aliquam amet, aperiam aspernatur commodi culpa delectus dignissimos dolorum ea eos eum facilis hic impedit inventore ipsam iste laborum minima mollitia nam nihil, nobis omnis optio pariatur perspiciatis quae qui quos repudiandae rerum saepe sint, sit soluta unde veniam vero vitae. Asperiores corporis omnis provident quo repellendus veritatis!';
-
-export default () => (
+const About = ({ title, text }) => (
   <section className={styles.about}>
     <div className="container container--text">
       <h2 className={`${headingStyles.primary} margin-bottom-40`}>
-        Par mani
+        {title}
       </h2>
       <div className={textStyles.text}>
-        <p>{TEXT}</p>
+        <p>{text}</p>
       </div>
     </div>
   </section>
 );
 
+About.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+export default connect(({ about: { title, text } }) => ({
+  title,
+  text,
+}))(About);
