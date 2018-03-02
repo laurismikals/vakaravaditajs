5327361abfd3cb721107d3fdcd81147bb6573b09
Lauris <lauris.mikals@gmail.com>
2018-03-02 13:31:50 +0200

    correct eslint errors

--- src/03-utilities/utils.js
+++ src/03-utilities/utils.js
@@ -1,40 +1,38 @@
-/* eslint-disable */
-import routesMap from '../routesMap'
-// import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
-export const isServer = typeof window === 'undefined'
+import routesMap from '../routesMap';
 
-export const fetchData = async (path, jwToken) =>
-  fetch(`http://localhost:3000${path}`, {
-    headers: {
-      Accept: 'application/json',
-      Authorization: `Bearer ${jwToken || ''}`
-    }
-  }).then(data => data.json())
-
-export const isAllowed = (type, state) => {
-  const role = routesMap[type] && routesMap[type].role // you can put arbitrary keys in routes
-  if (!role) return true
-
-  const user = isServer
-    ? jwt.verify(state.jwToken, process.env.JWT_SECRET)
-    : userFromState(state)
-
-  if (!user) return false
-
-  return user.roles.includes(role)
-}
+export const isServer = typeof window === 'undefined';
 
 // VERIFICATION MOCK:
 // since middleware is syncrhonous you must use a jwt package that is sync
 // like the one imported above. For now we will mock both the client + server
 // verification methods:
 
-const fakeUser = { roles: ['admin'] }
-const userFromState = ({ jwToken, user }) => jwToken === 'real' && fakeUser
-const jwt = {
-  verify: (jwToken, secret) => jwToken === 'real' && fakeUser
-}
+const fakeUser = { roles: ['admin'] };
+const userFromState = ({ jwToken }) => jwToken === 'real' && fakeUser;
+
+export const fetchData = async (path, jwToken) =>
+  fetch(`http://localhost:3000${path}`, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${jwToken || ''}`,
+    },
+  })
+    .then(data => data.json());
+
+export const isAllowed = (type, state) => {
+  const role = routesMap[type] && routesMap[type].role; // you can put arbitrary keys in routes
+  if (!role) return true;
+
+  const user = isServer
+    ? jwt.verify(state.jwToken, process.env.JWT_SECRET)
+    : userFromState(state);
+
+  if (!user) return false;
+
+  return user.roles.includes(role);
+};
 
 // NOTE ON COOKIES:
 // we're doing combination cookies + jwTokens because universal apps aren't
