9958f21e1e73ef28b9854fc4a01fe0581f7c8940
Lauris <lauris.mikals@gmail.com>
2018-03-02 13:27:33 +0200

    add service worker registration

--- src/06-components/03-layout/index.js
+++ src/06-components/03-layout/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 /* Global css that in source order comes before all other css */
 import '../../00-assets/css/index.css';
@@ -7,12 +7,31 @@ import HeaderMain from '../02-organisms/header-main/index';
 import FooterMain from '../02-organisms/footer-main/footer-main';
 import Switcher from './switcher';
 
-export default () => (
-  <div className="site">
-    <HeaderMain />
-    <main>
-      <Switcher />
-    </main>
-    <FooterMain />
-  </div>
-);
+export default class Layout extends Component {
+  componentDidMount() {
+    if ('serviceWorker' in navigator) {
+      // Register a service worker hosted at the root of the
+      // site using the default scope.
+      navigator.serviceWorker.register('/buildClient/service-worker.js')
+        .then((registration) => {
+          console.log('Service worker registration succeeded:', registration);
+        })
+        .catch((error) => {
+          console.log('Service worker registration failed:', error);
+        });
+    } else {
+      console.log('Service workers are not supported.');
+    }
+  }
+  render() {
+    return (
+      <div className="site">
+        <HeaderMain />
+        <main>
+          <Switcher />
+        </main>
+        <FooterMain />
+      </div>
+    );
+  }
+}
