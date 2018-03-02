8ca3ddeb21a5ba5a6c95c89e26225e60d4355d4b
Lauris <lauris.mikals@gmail.com>
2018-03-02 13:24:11 +0200

    correct eslint errors

--- src/options.js
+++ src/options.js
@@ -1,26 +1,25 @@
-/* eslint-disable */
-import { redirect } from 'redux-first-router'
-import { isAllowed, isServer } from './03-utilities/utils'
+import { redirect } from 'redux-first-router';
+import { isAllowed, isServer } from './03-utilities/utils';
 
 export default {
   onBeforeChange: (dispatch, getState, action) => {
-    const allowed = isAllowed(action.type, getState())
+    console.log('onAfterChange');
+    const allowed = isAllowed(action.type, getState());
 
     if (!allowed) {
-      const action = redirect({ type: 'LOGIN' })
-      dispatch(action)
+      dispatch(redirect({
+        type: 'LOGIN',
+      }));
     }
   },
   onAfterChange: (dispatch, getState) => {
-    const { type } = getState().location
+    console.log('onAfterChange');
+    const { type } = getState().location;
 
     if (type === 'LOGIN' && !isServer) {
       setTimeout(() => {
-        alert(alertMessage)
-      }, 1500)
+        alert("NICE, You're adventurous! Try changing the jwToken cookie from 'fake' to 'real' in server/index.js (and manually refresh) to access the Admin Panel. Then 'onBeforeChange' will let you in.");
+      }, 1500);
     }
-  }
-}
-
-const alertMessage =
-  "NICE, You're adventurous! Try changing the jwToken cookie from 'fake' to 'real' in server/index.js (and manually refresh) to access the Admin Panel. Then 'onBeforeChange' will let you in."
+  },
+};
