9e1c79d830b2938329687810a311e94bb0956093
Lauris <lauris.mikals@gmail.com>
2018-03-02 13:29:03 +0200

    add favicons

--- server/render.js
+++ server/render.js
@@ -13,7 +13,7 @@ const createApp = (App, store) => (
   </Provider>
 );
 
-export default ({ clientStats }) => async (req, res) => {
+export default ({ clientStats, outputPath }) => async (req, res) => {
   const store = await configureStore(req, res);
   if (!store) return; // no store means redirect was already served
 
@@ -25,6 +25,7 @@ export default ({ clientStats }) => async (req, res) => {
 
   console.log('REQUESTED PATH:', req.path); // eslint-disable-line no-console
   console.log('CHUNK NAMES', chunkNames); // eslint-disable-line no-console
+  console.log('outputPath', outputPath); // eslint-disable-line no-console
 
   return res.send(`<!doctype html>
     <html lang="lv">
@@ -37,13 +38,25 @@ export default ({ clientStats }) => async (req, res) => {
         <meta name="mobile-web-app-capable" content="yes">
         <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE">
         
-        <title>Boilerplate</title>
+        <title>Raivis Bružis - vakara vadītājs</title>
         
         ${styles}
         
         <!--link to the most important font which gets requested as soon as possible.
         this decreases possibility of flash of unstyled text.-->
         <link rel="preload" href="/fonts/subset-Roboto-Regular.woff2" as="font" type="font/woff2" crossorigin>
+        
+        <!--link to most important favicons-->
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#444444">
+        
+        <!--theme color-->
+        <meta name="theme-color" content="#444444">
+        
+        <!--link to manifest-->
+        <link rel="manifest" href="/manifest/manifest.json">
         
         <!--This service loads only the polyfills the browser needs-->
         <script src="//cdn.polyfill.io/v2/polyfill.min.js"></script>
