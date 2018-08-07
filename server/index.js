fa02de7575cc61d84c392da058279bc4e057792e
Lauris <lauris.mikals@gmail.com>
2018-08-07 16:57:02 +0300

    Remove dead code

--- server/index.js
+++ server/index.js
@@ -6,141 +6,43 @@ import webpackDevMiddleware from 'webpack-dev-middleware'; // eslint-disable-lin
 import webpackHotMiddleware from 'webpack-hot-middleware'; // eslint-disable-line import/no-extraneous-dependencies
 import webpackHotServerMiddleware from 'webpack-hot-server-middleware'; // eslint-disable-line import/no-extraneous-dependencies
 
-//AUTHENTICATION
+// Authentication
 import cookieParser from 'cookie-parser';
-import jwt from 'jsonwebtoken';
+import bodyParser from 'body-parser';
+import multer from 'multer';
+import session from 'express-session';
 
 const DEV = process.env.NODE_ENV === 'development';
 
+// WebPack config
 const webpackPath = DEV ? '../webpack/' : '../../webpack/';
 const clientConfig = require(`${webpackPath}client.dev`);
 const serverConfig = require(`${webpackPath}server.dev`);
-
-
-import { findVideos, findVideo } from './api';
-import db from './mysql/mysql';
 
 const { publicPath } = clientConfig.output;
 const outputPath = clientConfig.output.path;
 const app = express();
 
-app.use(compression());
-app.use(express.static('static'));
-
-// JWTOKEN COOKIE - in a real app obviously you set this after signup/login:
+// BodyParser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Multer
+const upload = multer();
 
-// app.use((req, res, next) => {
-//   const cookie = req.cookies.jwToken;
-//   const user = {
-//     name: "admin",
-//   };
-//   const jwToken = jwt.sign({user: user}, 'viss ir ok', (error, token) => {
-//     console.log('error', error);
-//     console.log('token', token);
-//   });
-//
-//   console.log('cookie', cookie);
-//   console.log('jwToken', jwToken);
-//
-//   if (cookie !== jwToken) {
-//     res.cookie('jwToken', jwToken, { maxAge: 900000 });
-//     req.cookies.jwToken = jwToken;
-//   }
-//
-//   next();
-// });
+// Compression
+app.use(compression());
 
-app.post('/api/login', (req, res) => {
-  const user = {
-    name: 'admin',
-  };
-  jwt.sign({user}, process.env.JWT_SECRET, (error, token) => {
-    console.log('error', error);
-    console.log('token', token);
-    res.json({
-      token
-    })
-  });
-});
+// Static folder
+app.use(express.static('static'));
 
-
-
-const verifyToken = (req, res, next) => {
-  //Get auth header value
-  const bearerHeader = req.headers['authorization'];
-  //Chech if bearer is undefined
-  if(!!bearerHeader){
-    //Split at the space
-    const bearer = bearerHeader.split(' ');
-    //Set token
-    req.token = bearer[1];
-    //Next midelware
-    next();
-  }else{
-    res.sendStatus(403);
-  }
-};
-
-app.post('/api/posts', verifyToken, (req, res) => {
-  jwt.verify(req.token, process.env.JWT_SECRET, (error, authData) => {
-    if(error){
-      res.sendStatus(403);
-    }else {
-      res.json({
-        message: 'Post created',
-        authData,
-      })
-    }
-  });
-});
-
-// API
-
-app.get('/abouttable', (req, res) => {
-  const sql = 'CREATE TABLE about(title VARCHAR(255), text TEXT)';
-  db.query(sql, (error, result) => {
-    if(error) throw error;
-    res.send('About table created...');
-  });
-});
-
-app.get('/api/update/about', (req, res) => {
-  const about = {
-    title: 'Par mani2',
-    text: '2Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque eos eum ex ipsa nostrum possimus repudiandae, totam veniam veritatis! A ad adipisci aliquam amet, aperiam aspernatur commodi culpa delectus dignissimos dolorum ea eos eum facilis hic impedit inventore ipsam iste laborum minima mollitia nam nihil, nobis omnis optio pariatur perspiciatis quae qui quos repudiandae rerum saepe sint, sit soluta unde veniam vero vitae. Asperiores corporis omnis provident quo repellendus veritatis!',
-  };
-
-  const sql = `UPDATE about SET
-    title = '${about.title}',
-    text = '${about.text}'
-    WHERE id = 1`;
-  db.query(sql, (error, result) => {
-    if(error) throw error;
-    res.send('About changed');
-  });
-});
-
-app.get('/api/get/about', (req, res) => {
-  const sql = 'SELECT * FROM about';
-  db.query(sql, (error, result) => {
-    if(error) throw error;
-    res.json(result[0]);
-  });
-});
-
-app.get('/api/videos/:category', async (req, res) => {
-  const jwToken = req.headers.authorization.split(' ')[1];
-  const data = await findVideos(req.params.category, jwToken);
-  res.json(data);
-});
-
-app.get('/api/video/:slug', async (req, res) => {
-  const jwToken = req.headers.authorization.split(' ')[1];
-  const data = await findVideo(req.params.slug, jwToken);
-  res.json(data);
-});
+// Express session
+app.use(session({
+  secret: process.env.AUTHENTICATION_SECRET,
+  saveUninitialized: true,
+  resave: true,
+}));
 
 // UNIVERSAL HMR + STATS HANDLING GOODNESS:
 
