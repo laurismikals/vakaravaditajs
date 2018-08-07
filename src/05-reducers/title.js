85246b0c69c968ac83d851ca9ccdccb9cbf32a12
Lauris <lauris.mikals@gmail.com>
2018-08-07 17:09:15 +0300

    Remove dead code

--- src/05-reducers/title.js
+++ src/05-reducers/title.js
@@ -1,22 +1,8 @@
-/* eslint-disable */
 export default (state = 'Raivis Bružis - vakara vadītājs', action = {}) => {
   switch (action.type) {
     case 'HOME':
       return 'Raivis Bružis - vakara vadītājs';
-    case 'LIST':
-      return `RFR: ${capitalize(action.payload.category)}`;
-    case 'VIDEO':
-      return `RFR: ${capitalize(action.payload.slug)}`;
-    case 'LOGIN':
-      return 'RFR Login';
-    case 'ADMIN':
-      return 'RFR Admin';
     default:
       return state;
   }
 };
-
-const capitalize = str =>
-  str.replace(/-/g, ' ').replace(/\b\w/g, l => l.toUpperCase());
-
-// RFR automatically changes the document.title for you :)
