9b8d25f95fc457a9ad16ad176a1017cb1599ba15
Lauris <lauris.mikals@gmail.com>
2018-08-07 16:56:44 +0300

    Add image to contacts

--- src/06-views/00-components/02-organisms/contact/contact.js
+++ src/06-views/00-components/02-organisms/contact/contact.js
@@ -1,21 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import Image from '../../00-atoms/image/image';
 
 import headingStyles from '../../00-atoms/heading/heading.css';
 import styles from './contact.css';
 
-export default () => (
+const Contact = ({ image }) => (
   <section className={styles.contact}>
-    <div className="container">
-      <h2 className={`${headingStyles.primary} margin-bottom-40 text-align--center`}>
-        Sāksim ar sarunu
-      </h2>
-      <a
-        href="tel:+37128266845"
-        className={styles.phone}
-      >
-        +371 28 266 845
-      </a>
+    {image &&
+      <Image
+        src="/images/raivis-2"
+        alt="Vakara vadītājs Raivis Bružis"
+        cover
+        className={styles.image}
+      />
+    }
+    <div className={styles.container}>
+      <div className="container">
+        <h2 className={`${styles.heading} ${headingStyles.primary} margin-bottom-40 text-align--center`}>
+          Sāksim ar sarunu
+        </h2>
+        <a
+          href="tel:+37128266845"
+          className={styles.phone}
+        >
+          +371 28 266 845
+        </a>
+      </div>
     </div>
   </section>
 );
 
+Contact.propTypes = {
+  image: PropTypes.bool,
+};
+Contact.defaultProps = {
+  image: false,
+};
+
+export default Contact;
