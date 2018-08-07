58a69fcd22a52da232cbb7d03957d734dd70fc08
Lauris <lauris.mikals@gmail.com>
2018-08-07 16:57:56 +0300

    Remove unneeded reducers

--- src/05-reducers/index.js
+++ src/05-reducers/index.js
@@ -1,13 +1,4 @@
-/* eslint-disable */
-export { default as page } from './page'
-export { default as slug } from './slug'
-export { default as category } from './category'
-export { default as videosHash } from './videosHash'
-export { default as videosByCategory } from './videosByCategory'
-export { default as playing } from './playing'
-export { default as user } from './user'
-export { default as title } from './title'
-export { default as actions } from './actions'
-export { default as jwToken } from './jwToken'
-export { default as posts } from './posts'
-export { default as about } from './about'
+export { default as page } from './page';
+export { default as slug } from './slug';
+export { default as title } from './title';
+export { default as actions } from './actions';
