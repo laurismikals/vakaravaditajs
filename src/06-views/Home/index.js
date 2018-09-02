ea17890b6a7ef4d5a9700509635d104c2dce9629
Lauris <lauris.mikals@gmail.com>
2018-09-02 13:34:31 +0300

    Replace div with Fragment

--- src/06-views/Home/index.js
+++ src/06-views/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import Head from '../00-components/02-organisms/head/head';
 import About from '../00-components/02-organisms/about/about';
@@ -7,11 +7,11 @@ import Services from '../00-components/02-organisms/services/services';
 import Contact from '../00-components/02-organisms/contact/contact';
 
 export default () => (
-  <div>
+  <Fragment>
     <Head />
     <About />
     <Testimonials />
     <Services />
     <Contact />
-  </div>
+  </Fragment>
 );
