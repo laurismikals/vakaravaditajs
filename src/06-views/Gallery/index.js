354f5235fc464b0209f6efcd5547b477949ffc37
Lauris <lauris.mikals@gmail.com>
2018-09-02 13:34:14 +0300

    Render gallery thumbnails

--- src/06-views/Gallery/index.js
+++ src/06-views/Gallery/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
+import React, { Fragment, PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { map } from 'lodash';
+import { connect } from 'react-redux';
 
-import Head from '../00-components/02-organisms/head/head';
-import About from '../00-components/02-organisms/about/about';
-import Testimonials from '../00-components/02-organisms/testimonials/testimonials';
-import Services from '../00-components/02-organisms/services/services';
-import Contact from '../00-components/02-organisms/contact/contact';
+import PageHeader from '../00-components/01-molecules/page-header/page-header';
+import CardGallery from '../00-components/01-molecules/card-gallery/card-gallery';
 
-export default () => (
-  <div>
-    <Services />
-    <Testimonials />
-    <Head />
-    <About />
-    <Contact />
-  </div>
-);
+const galleryArr = [
+  {
+    href: '/gallery/0',
+    image: '/images/head-1',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipisicing',
+  },
+  {
+    href: '/gallery/1',
+    image: '/images/raivis',
+    title: 'Consequuntur dolore doloremque dolores enim eum',
+  },
+  {
+    href: '/gallery/2',
+    image: '/images/raivis',
+    title: 'Possimus quas tempora velit voluptatem',
+  },
+  {
+    href: '/gallery/3',
+    image: '/images/head-1',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipisicing',
+  },
+];
+
+class Gallery extends PureComponent {
+  static propTypes = {
+    id: PropTypes.number,
+  };
+
+  render() {
+    const { id } = this.props;
+
+    return (
+      <Fragment>
+        {id}
+        <PageHeader
+          className="margin-bottom-50"
+          heading="Galerija"
+        />
+        <div className="container">
+          <div className="width-100 margin-bottom-30">
+            <div className="row row--gutters-20">
+              {galleryArr && map(galleryArr, ({
+                href,
+                image,
+                title,
+                text,
+              }, i) => (
+                <div key={i} className="col-s1-24 col-s3-12 col-s5-8 col-s6-6">
+                  <CardGallery
+                    className="margin-bottom-20"
+                    href={href}
+                    image={image}
+                    title={title}
+                    text={text}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+const mapState = ({ location: { payload: { id } } }) => ({
+  id,
+});
+
+export default connect(mapState)(Gallery);
