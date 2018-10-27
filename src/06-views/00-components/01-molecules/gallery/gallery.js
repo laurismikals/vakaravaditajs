32bbe4cfd8eb9badc18bd68b4d207e995f1d6d3d
Lauris <lauris.mikals@gmail.com>
2018-10-27 21:52:20 +0300

    Add gallery

--- src/06-views/00-components/01-molecules/gallery/gallery.js
+++ src/06-views/00-components/01-molecules/gallery/gallery.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { Portal } from 'react-portal';
+import Portal from 'react-portal-minimal';
 import { push } from 'redux-first-router';
 
 import getScrollbarWidth from '../../../../00-assets/js/get-scrollbar-width';
@@ -12,195 +12,121 @@ import styles from './gallery.css';
 
 const isBrowser = () => typeof window !== 'undefined' && !!window.document;
 
-const imageArr = {
-  0: '/images/head-1',
-  1: '/images/raivis',
-  2: '/images/raivis',
-  3: '/images/head-1',
+const useDisableScroll = () => {
+  const DIALOG_OPENED_CLASS = 'state-dialog-opened';
+  const body = isBrowser() && window.document.body;
+  const htmlElement = isBrowser() && window.document.documentElement;
+  let scrollPositionWhenDialogWasOpened;
+
+  const disable = () => {
+    scrollPositionWhenDialogWasOpened = htmlElement.scrollTop || body.scrollTop;
+    body.style.top = `-${scrollPositionWhenDialogWasOpened}px`;
+    body.style.height = isBrowser() && `${window.innerHeight}px`;
+    body.style.paddingRight = `${getScrollbarWidth()}px`;
+
+    body.classList.add(DIALOG_OPENED_CLASS);
+    htmlElement.classList.add(DIALOG_OPENED_CLASS);
+  };
+
+  const enable = () => {
+    body.classList.remove(DIALOG_OPENED_CLASS);
+    htmlElement.classList.remove(DIALOG_OPENED_CLASS);
+
+    body.style.height = '100%';
+    body.style.top = '0px';
+    body.style.paddingRight = '0px';
+    htmlElement.scrollTop = scrollPositionWhenDialogWasOpened;
+    body.scrollTop = scrollPositionWhenDialogWasOpened;
+  };
+
+  useEffect(() => {
+    disable();
+    return enable;
+  });
 };
 
-const DIALOG_OPENED_CLASS = 'state-dialog-opened';
+const useClick = onClick => useEffect(() => {
+  setTimeout(() => window.addEventListener('click', onClick), 0);
+  return () => window.removeEventListener('click', onClick);
+});
 
-export class Gallery extends Component {
-  static propTypes = {
-    id: PropTypes.oneOfType([
-      PropTypes.number,
-      PropTypes.string,
-      PropTypes.bool,
-    ]).isRequired,
-    open: PropTypes.bool.isRequired,
-  };
+const keepInBetween = (min, max) => (number) => {
+  if (number < min) { return max; }
+  if (number > max) { return min; }
+  return number;
+};
 
-  state = { mountChildren: false };
+export const Gallery = ({
+  open,
+  id,
+  galleryArr,
+}) => {
+  const content = useRef(null);
 
-  body = isBrowser() && window.document.body;
-
-  htmlElement = isBrowser() && window.document.documentElement;
-
-  componentDidMount() {
-    const { open } = this.props;
-    if (open) {
-      this.openState();
-    }
-    if (isBrowser()) {
-      window.addEventListener('click', this.handleClickOutside);
-    }
-    this.setState({ mountChildren: true });
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.open) {
-      const { open } = this.props;
-      if (!open) {
-        this.openState();
-      }
-    } else {
-      this.closeState();
-    }
-  }
-
-  componentWillUnmount() {
-    if (isBrowser()) {
-      window.removeEventListener('click', this.handleClickOutside);
-    }
-    this.closeState();
-  }
-
-  onCloseHandler = () => {
-    this.animateClose(() => push('/gallery'));
-  };
-
-  handleClickOutside = (e) => {
-    if (this.content && !this.content.contains(e.target)) {
-      this.onCloseHandler();
+  const handleClickOutside = (e) => {
+    if (content.current && !content.current.contains(e.target)) {
+      push('/gallery');
     }
   };
 
-  animateClose(callback) {
-    const options = {
-      duration: 300,
-      easing: 'ease-in-out',
-    };
-    const keyframesDialog = [
-      {
-        visibility: 'visible',
-        opacity: 1,
-      },
-      {
-        visibility: 'hidden',
-        opacity: 0,
-      },
-    ];
-    const keyframesContent = [
-      {
-        visibility: 'visible',
-        opacity: 1,
-        transform: 'translateY(0px)',
-      },
-      {
-        visibility: 'hidden',
-        opacity: 0,
-        transform: 'translateY(-20px)',
-      },
-    ];
+  useClick(handleClickOutside);
+  useDisableScroll();
 
-    this.dialog.style.visibility = 'hidden';
-    this.dialog.style.opacity = 0;
-    if (this.content.animate) {
-      this.dialog.animate(keyframesDialog, options);
-      const animationContent = this.content.animate(keyframesContent, options);
-      animationContent.onfinish = callback;
-    } else {
-      callback();
-    }
-  }
+  const prevHandler = () => push(`/gallery/${keepInBetween(0, galleryArr.length - 1)(id - 1)}`);
+  const nextHandler = () => push(`/gallery/${keepInBetween(0, galleryArr.length - 1)(id + 1)}`);
 
-  animateOpen() {
-    const options = {
-      duration: 300,
-      easing: 'ease-in-out',
-    };
-    const keyframesDialog = [
-      {
-        visibility: 'hidden',
-        opacity: 0,
-      },
-      {
-        visibility: 'visible',
-        opacity: 1,
-      },
-    ];
-    const keyframesContent = [
-      {
-        visibility: 'hidden',
-        opacity: 0,
-        transform: 'translateY(-20px)',
-      },
-      {
-        visibility: 'visible',
-        opacity: 1,
-        transform: 'translateY(0px)',
-      },
-    ];
-
-    this.dialog.style.visibility = 'visible';
-    this.dialog.style.opacity = 1;
-    if (this.content.animate) {
-      this.dialog.animate(keyframesDialog, options);
-      this.content.animate(keyframesContent, options);
-    }
-  }
-
-  openState() {
-    this.scrollPositionWhenDialogWasOpened = this.htmlElement.scrollTop || this.body.scrollTop;
-    this.body.style.top = `-${this.scrollPositionWhenDialogWasOpened}px`;
-    this.body.style.height = isBrowser() && `${window.innerHeight}px`;
-    this.body.style.paddingRight = `${getScrollbarWidth()}px`;
-
-    this.body.classList.add(DIALOG_OPENED_CLASS);
-    this.htmlElement.classList.add(DIALOG_OPENED_CLASS);
-
-    this.animateOpen();
-  }
-
-  closeState() {
-    this.body.classList.remove(DIALOG_OPENED_CLASS);
-    this.htmlElement.classList.remove(DIALOG_OPENED_CLASS);
-
-    this.body.style.height = '100%';
-    this.body.style.top = '0px';
-    this.body.style.paddingRight = '0px';
-    this.htmlElement.scrollTop = this.scrollPositionWhenDialogWasOpened;
-    this.body.scrollTop = this.scrollPositionWhenDialogWasOpened;
-  }
-
-  render() {
-    const { id } = this.props;
-    const { mountChildren } = this.state;
-
-    return (
-      <Portal>
-        <div />
-        <dialog
-          ref={(element) => { this.dialog = element; }}
-          className={styles.root}
+  return (
+    <Portal>
+      <dialog
+        className={styles.root}
+        open={open}
+      >
+        <button
+          type="button"
+          className={styles.close}
         >
-          <button
-            type="button"
-            className={styles.close}
+          <Icon icon="close" />
+        </button>
+        <div className={`${styles.container} container`}>
+          <div
+            ref={content}
+            className={styles.content}
           >
-            <Icon icon="close" />
-          </button>
-          <div className={`${styles.container} container`}>
-            <div
-              ref={(element) => { this.content = element; }}
-              className={styles.content}
-            >
-              {mountChildren && <Image src={imageArr[id]} alt="" />}
+            <div className={styles.imageWrap}>
+              <Image
+                src={galleryArr[id].image}
+                alt={galleryArr[id].title}
+                cover
+                className={styles.image}
+              />
             </div>
+            <button
+              type="button"
+              className={`${styles.arrow} ${styles.arrowLeft}`}
+              onClick={prevHandler}
+            >
+              <Icon icon="arrow-short-left" />
+            </button>
+            <button
+              type="button"
+              className={`${styles.arrow} ${styles.arrowRight}`}
+              onClick={nextHandler}
+            >
+              <Icon icon="arrow-short-right" />
+            </button>
           </div>
-        </dialog>
-      </Portal>
-    );
-  }
-}
+        </div>
+      </dialog>
+    </Portal>
+  );
+};
+
+Gallery.propTypes = {
+  open: PropTypes.bool.isRequired,
+  id: PropTypes.number.isRequired,
+  galleryArr: PropTypes.arrayOf(PropTypes.shape({
+    href: PropTypes.string,
+    image: PropTypes.string,
+    title: PropTypes.string,
+  })).isRequired,
+};
